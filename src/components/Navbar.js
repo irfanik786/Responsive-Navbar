73dac46026397a84b50ea4909cf3a07225af457c
irfanik786 <irfansure77@gmail.com>
2024-07-23 12:26:31 +0530

    added all files

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react'
+import {FaBars, FaTimes} from "react-icons/fa";
+import '../styles/main.css';
+
+const Navbar = () => {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+  return (                                  // npm install react-icons --save :- for react icons
+    <header>
+        <h3>Logo</h3>
+        <nav ref={navRef}>
+            <a href="/#">Home</a>
+            <a href="/#">My Work</a>
+            <a href="/#">Blog</a>
+            <a href="/#">About Me</a>
+            <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+                <FaTimes />
+            </button>
+        </nav>
+        <button className='nav-btn' onClick={showNavbar}>
+            <FaBars />
+        </button>
+    </header>
+  )
+}
+
+export default Navbar
